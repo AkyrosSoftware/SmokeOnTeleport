89fbbfd574fbd164d12b55794dee874e22def64d
David Derthick <25013744+DDerthickJr@users.noreply.github.com>
2022-01-30 17:27:38 -0500

    Yaml Config Part 2

--- src/main/java/com/akyrossoftware/plugins/smokeonteleport/ConfigHandler.java
+++ src/main/java/com/akyrossoftware/plugins/smokeonteleport/ConfigHandler.java
@@ -1,22 +1,17 @@
 package com.akyrossoftware.plugins.smokeonteleport;
 
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.configuration.file.FileConfiguration;
+
 
 public class ConfigHandler {
 
-    private YamlConfiguration yamlFile;
+    private FileConfiguration yamlFile;
     private SmokeOnTeleport plugin;
 
     public ConfigHandler(SmokeOnTeleport plugin){
         this.plugin = plugin;
-        yamlFile = new YamlConfiguration();
-        try{
-            yamlFile.load(plugin.getConfig().getCurrentPath());
-        }
-        catch(Exception ex){
-            plugin.getLogger().config(ex.getMessage());
-        }
+        yamlFile = plugin.getConfig();
     }
 
     public ConfigurationSection getConfigSection(){
--- src/main/java/com/akyrossoftware/plugins/smokeonteleport/SmokeOnTeleport.java
+++ src/main/java/com/akyrossoftware/plugins/smokeonteleport/SmokeOnTeleport.java
@@ -1,7 +1,6 @@
 package com.akyrossoftware.plugins.smokeonteleport;
 
 import org.bukkit.Particle;
-import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
