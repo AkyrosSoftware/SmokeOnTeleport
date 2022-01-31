f439e01c52576430750b076de1c4db26616bfb5b
David Derthick <25013744+DDerthickJr@users.noreply.github.com>
2022-01-31 08:55:04 -0500

    Yaml Config Part 3

--- src/main/java/com/akyrossoftware/plugins/smokeonteleport/ConfigHandler.java
+++ src/main/java/com/akyrossoftware/plugins/smokeonteleport/ConfigHandler.java
@@ -1,20 +1,50 @@
 package com.akyrossoftware.plugins.smokeonteleport;
 
+import java.io.File;
+import java.io.IOException;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 
 public class ConfigHandler {
 
-    private FileConfiguration yamlFile;
+    private YamlConfiguration yamlFile;
     private SmokeOnTeleport plugin;
 
     public ConfigHandler(SmokeOnTeleport plugin){
         this.plugin = plugin;
-        yamlFile = plugin.getConfig();
+        yamlFile = new YamlConfiguration();
+    }
+    
+    public void loadConfig(){
+        File folder = createDataFolder();
+        File config = new File(folder, "config.yml");
+        if (!config.exists()) {
+            plugin.getLogger().info("No config-file found. Creating default...");
+            plugin.saveDefaultConfig();
+        }
+        try
+        {
+            yamlFile.load(config);
+        }
+        catch (IOException | InvalidConfigurationException ex)
+        {
+            throw new IllegalStateException("Failed to load config-file", ex);
+        }
     }
 
     public ConfigurationSection getConfigSection(){
         return yamlFile.getConfigurationSection("particle-effects");
+    }
+    
+    private File createDataFolder() {
+        File folder = plugin.getDataFolder();
+        if (!folder.exists()) {
+            if (!folder.mkdirs()) {
+                throw new IllegalStateException("Failed to create data folder");
+            }
+        }
+        return folder;
     }
 }
--- src/main/java/com/akyrossoftware/plugins/smokeonteleport/SmokeOnTeleport.java
+++ src/main/java/com/akyrossoftware/plugins/smokeonteleport/SmokeOnTeleport.java
@@ -5,17 +5,19 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SmokeOnTeleport extends JavaPlugin {
+    
+    private ConfigHandler configHandler;
 
     @Override
     public void onEnable() {
         PluginManager pm = this.getServer().getPluginManager();
-        ConfigHandler yamlHandler = new ConfigHandler(this);
-        double offsetX = yamlHandler.getConfigSection().getDouble("offsetX", .01);
-        double offsetY = yamlHandler.getConfigSection().getDouble("offsetY", .01);
-        double offsetZ = yamlHandler.getConfigSection().getDouble("offsetZ", .01);
-        double speed = yamlHandler.getConfigSection().getDouble("speed", .01);
-        int count = yamlHandler.getConfigSection().getInt("count", 25);
-        String particleName = yamlHandler.getConfigSection().getString("particle", "SMOKE_NORMAL");
+        reloadConfig();
+        double offsetX = configHandler.getConfigSection().getDouble("offsetX", .01);
+        double offsetY = configHandler.getConfigSection().getDouble("offsetY", .01);
+        double offsetZ = configHandler.getConfigSection().getDouble("offsetZ", .01);
+        double speed = configHandler.getConfigSection().getDouble("speed", .01);
+        int count = configHandler.getConfigSection().getInt("count", 25);
+        String particleName = configHandler.getConfigSection().getString("particle", "SMOKE_NORMAL");
         Particle particle = Particle.valueOf(particleName);
         TeleportListener listener = new TeleportListener(
                 this, particle, count,
@@ -27,5 +29,13 @@ public final class SmokeOnTeleport extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    
+    @Override
+    public void reloadConfig(){
+        if(configHandler == null){
+            configHandler = new ConfigHandler(this);
+        }
+        configHandler.loadConfig();
     }
 }
