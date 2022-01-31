80112876d676243eb0d4a4369f9557bad928fce2
David Derthick <25013744+DDerthickJr@users.noreply.github.com>
2022-01-31 16:16:38 -0500

    Added TabComplete

--- src/main/java/com/akyrossoftware/plugins/smokeonteleport/CommandHandler.java
+++ src/main/java/com/akyrossoftware/plugins/smokeonteleport/CommandHandler.java
@@ -1,16 +1,31 @@
 package com.akyrossoftware.plugins.smokeonteleport;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.Particle;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 
 
-public class CommandHandler implements CommandExecutor
+public class CommandHandler implements CommandExecutor, TabCompleter
 {
     private SmokeOnTeleport plugin;
+    private List<String> commands;
+    private List<String> particles;
     
     public CommandHandler(SmokeOnTeleport plugin){
         this.plugin = plugin;
+        commands = new ArrayList<>();
+        commands.add("reload");
+        commands.add("particle");
+        particles = new ArrayList<>();
+        Particle[] enumList = Particle.values();
+        for (Particle particle : enumList)
+        {
+            particles.add(particle.toString().toUpperCase());
+        }
     }
 
     @Override
@@ -26,6 +41,23 @@ public class CommandHandler implements CommandExecutor
                     plugin.reloadConfig();
                     return true;
                 }
+                case "particle":{
+                    if(strings.length >= 2){
+                        String nextArg = strings[1].toUpperCase();
+                        if(particles.contains(nextArg)){
+                            plugin.getConfig().getConfigurationSection("particle-effects").set("particle", nextArg);
+                            plugin.saveConfig();
+                            plugin.reloadConfig();
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        return false;
+                    }
+                }
                 default:{
                     return false;
                 }
@@ -33,6 +65,20 @@ public class CommandHandler implements CommandExecutor
         }
         else{
             return false;
+        }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender cs, Command cmnd, String string, String[] strings)
+    {
+        if(strings.length == 0){
+            return commands;
+        }
+        else if(strings[0].equals("particle")){
+            return particles;
+        }
+        else{
+            return null;
         }
     }
 }
--- src/main/java/com/akyrossoftware/plugins/smokeonteleport/SmokeOnTeleport.java
+++ src/main/java/com/akyrossoftware/plugins/smokeonteleport/SmokeOnTeleport.java
@@ -6,13 +6,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class SmokeOnTeleport extends JavaPlugin {
     
     private ConfigHandler configHandler;
+    private CommandHandler commandHandler;
 
     @Override
     public void onEnable() {
         PluginManager pm = this.getServer().getPluginManager();
         reloadConfig();
         
-        this.getCommand("smoketp").setExecutor(new CommandHandler(this));
+        commandHandler = new CommandHandler(this);
+        this.getCommand("smoketp").setExecutor(commandHandler);
+        this.getCommand("smoketp").setTabCompleter(commandHandler);
         
         TeleportListener listener = new TeleportListener(
                 this, configHandler
